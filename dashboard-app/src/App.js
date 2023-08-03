319a8f0f0d893ca0c18212becfb4300f08a6b565
Jack Nortje <jack@bluegrassdigital.com>
2023-08-03 11:05:13 +0200

    New-repo-created

---
+++ dashboard-app/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ require('./Mat.jpg')} alt='' />
+        <p>
+          Welcome to Mats Dashboard Repo
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
